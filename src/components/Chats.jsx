043dfefea5af4dcf6cfe47b90e6fa22375df3554
ParthGupta84616 <148619447+ParthGupta84616@users.noreply.github.com>
2024-03-05 01:20:47 +0530

    Finding And Adding new users

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,21 +1,46 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { useEffect, useState } from 'react'
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from '../firebase';
+import { AuthContext } from '../Context/AuthContext';
 function Chats() {
+  const [chats, setChats] = useState([])
+  const { currentUser }= useContext(AuthContext)
+    
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+  console.log(Object.entries(chats));
   return (
+    
+    
     <div>
-      <div className="absolute flex bg-slate-800 w-1/4 p-4 rounded-lg ">
+      {Object.entries(chats)?.map((chat)=>(
+        <div key={chat[0]} className="relative flex bg-slate-800 p-4 rounded-lg ">
         <img
-          src="https://th.bing.com/th?id=ORMS.8ffbda0a4d3dd77c25111cd220429d7f&pid=Wdp&w=612&h=304&qlt=90&c=1&rs=1&dpr=1.25&p=0" // Replace with the correct path to your avatar image
+          src={chat[1].userInfo.photoURL} // Replace with the correct path to your avatar image
           alt="Avatar"
           className="w-16 h-16 rounded-full"
         />
         <div className="ml-5 text-12 text-white flex-col items-center">
-          <div className="font-bold">Lorem Ipsum</div>
-          <div><p className='text-sm'>Hello Yo</p></div>
+          <div className="font-bold">{chat[1].userInfo.name}</div>
+          <div><p className='text-sm'>{chat[1].userInfo.lastMessage?.text}</p></div>
         </div>
       </div>  
-      iopyioyioy
+        ))}
+        
     </div>
+    
   )
 }
 
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { db } from '../firebase';
-import { collection, query, where, getDocs } from 'firebase/firestore';
+import { collection, query, where, getDocs, setDoc  ,doc,updateDoc, serverTimestamp, getDoc } from 'firebase/firestore';
+import { AuthContext } from '../Context/AuthContext';
 
 function Searchbar() {
   const [username, setUsername] = useState('');
   const [user, setUser] = useState(null);
   const [error, setError] = useState(false);
+  const {currentUser} = useContext(AuthContext)
 
   const handleSearch = async () => {
     try {
@@ -32,7 +34,39 @@ function Searchbar() {
       handleSearch();
     }
   };
+  const handleSelect = async () => {
+    const combinedID = currentUser.uid > user.uid ? currentUser.uid + user.uid : user.uid + currentUser.uid;
+    try {
+        const res = await getDoc(doc(db, 'Chats', combinedID)); // Corrected the usage of getDocs
+        if (!res.exists()) {
+            await setDoc(doc(db, 'chats', combinedID), {
+                messages: []
+            });
+            await updateDoc(doc(db, 'userChats', currentUser.uid), {
+                [combinedID + '.userInfo']: {
+                    uid: user.uid,
+                    name: user.name,
+                    photoURL: user.photoURL
+                },
+                [combinedID + '.date']: serverTimestamp()
+            });
+            await updateDoc(doc(db, 'userChats', user.uid), {
+              [combinedID + '.userInfo']: {
+                  uid: currentUser.uid,
+                  name: currentUser.name,
+                  photoURL: currentUser.photoURL
+              },
+              [combinedID + '.date']: serverTimestamp()
+          });
+        }
+    } catch (error) {
+        console.error('Error creating or updating chat:', error);
+    }
+    setUser(null)
+    setUsername("")
+};
 
+  
   return (
     <div>
       <input
@@ -40,11 +74,12 @@ function Searchbar() {
         onKeyDown={handleKey}
         onChange={(e) => setUsername(e.target.value)}
         placeholder="Find User"
+        value={username}
         className="w-full h-10 px-4 bg-slate-700 text-black text-xl focus:outline-none focus:bg-pink-400 focus:text-gray-900"
       />
       {error && <span>User Not Found!</span>}
       {user && (
-        <div className="flex bg-slate-800 p-4 rounded-lg ">
+        <div className="flex bg-slate-800 p-4 rounded-lg " onClick={handleSelect}>
           <img
             src={user.photoURL}
             alt="Avatar"
