23afb386eb056d886004ff1f6c08d9fb0204cde0
ParthGupta84616 <148619447+ParthGupta84616@users.noreply.github.com>
2024-03-06 23:43:08 +0530

    Final And ready To Deploy

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -30,23 +30,33 @@ function Chats() {
     });
   };
 
-  return (
-    <div>
-      {Object.entries(chats).sort((a, b) => b[1].date - a[1].date).map((chat) => (
-        <div key={chat[0]} onClick={() => handleSelect(chat[1].userInfo)} className="relative flex bg-slate-800 p-4 rounded-lg">
-          <img
-            src={chat[1].userInfo?.photoURL || 'fallback_url'}
-            alt="Avatar"
-            className="w-16 h-16 rounded-full"
-          />
-          <div className="ml-5 text-12 text-white flex-col items-center">
-            <div className="font-bold">{chat[1].userInfo?.name}</div> 
-            <div><p className='text-sm'>{chat[1].lastMessage?.text}</p></div>
+    return (
+      <div>
+        {Object.entries(chats).sort((a, b) => b[1].date - a[1].date).map((chat) => (
+          <div key={chat[0]} onClick={() => handleSelect(chat[1].userInfo)} className="relative flex bg-slate-800 p-4 rounded-lg">
+            <img
+              src={chat[1].userInfo?.photoURL || 'fallback_url'}
+              alt="Avatar"
+              className="w-16 h-16 rounded-full"
+            />
+            <div className="ml-5 text-12 text-white flex-col items-center">
+              <div className="font-bold">{chat[1].userInfo?.name}</div> 
+              {chat[1].lastMessage?.text === "" ? (
+               
+                <div>
+                  <h1>Continue!</h1>
+                </div>
+              ) : (
+                <div>
+                  <p className='text-sm'>{chat[1].lastMessage?.text}</p>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
+        ))}
+      </div>
+    );
+    
 }
 
 export default Chats;
